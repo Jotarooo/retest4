597f1b761f610c64d3ad18a5d5ed6e4a8db4d78f
jotarooo <merrylmaitihe@gmail.com>
2021-04-30 16:07:33 -0930

    Initial commit yes

---
+++ app/src/main/java/com/example/retest4/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.retest4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button play;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.play = (Button) findViewById(R.id.button);
+        play.setOnClickListener(view -> {
+            Intent autreact = new Intent (getApplicationContext(), other.class);
+            startActivity(autreact);
+            finish();
+        });
+    }
+}
