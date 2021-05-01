b0b1fede91f92680ea5287d14526916e4a241191
jotarooo <merrylmaitihe@gmail.com>
2021-04-30 17:09:04 -0930

    ajout des bouttons retour modified

--- app/src/main/java/com/example/retest4/coloriageActivity.java
+++ app/src/main/java/com/example/retest4/coloriageActivity.java
@@ -2,13 +2,24 @@ package com.example.retest4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class coloriageActivity extends AppCompatActivity {
+
+    private Button retour;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coloriage);
+        this.retour = (Button) findViewById(R.id.button2);
+        retour.setOnClickListener(view -> {
+            Intent retourArriere = new Intent (getApplicationContext(), MainActivity.class);
+            startActivity(retourArriere);
+            finish();
+        });
     }
 }
