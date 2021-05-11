dd233507854847d77de680375d6dae141cdff57d
jotarooo <merrylmaitihe@gmail.com>
2021-05-11 10:26:25 -0930

    11/05 10h25

--- app/src/main/java/com/example/retest4/MainActivity.java
+++ app/src/main/java/com/example/retest4/MainActivity.java
@@ -16,42 +16,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.play = (Button) findViewById(R.id.button);
+        this.play = findViewById(R.id.button);
         play.setOnClickListener(view -> {
             Intent autreact = new Intent (getApplicationContext(), Chiffre.class);
             startActivity(autreact);
             finish();
         });
 
-        this.play = (Button) findViewById(R.id.button5);
+        this.play = findViewById(R.id.button5);
         play.setOnClickListener(view -> {
             Intent autreact = new Intent (getApplicationContext(), CouleurActivity.class);
             startActivity(autreact);
             finish();
         });
 
-        this.play = (Button) findViewById(R.id.button6);
+        this.play = findViewById(R.id.button6);
         play.setOnClickListener(view -> {
             Intent autreact = new Intent (getApplicationContext(), numeraActivity.class);
             startActivity(autreact);
             finish();
         });
 
-        this.play = (Button) findViewById(R.id.button7);
+        this.play = findViewById(R.id.button7);
         play.setOnClickListener(view -> {
             Intent autreact = new Intent (getApplicationContext(), formeActivity.class);
             startActivity(autreact);
             finish();
         });
 
-        this.play = (Button) findViewById(R.id.button3);
+        this.play = findViewById(R.id.button3);
         play.setOnClickListener(view -> {
             Intent autreact = new Intent (getApplicationContext(), lettreActivity.class);
             startActivity(autreact);
             finish();
         });
 
-        this.play = (Button) findViewById(R.id.button4);
+        this.play = findViewById(R.id.button4);
         play.setOnClickListener(view -> {
             Intent autreact = new Intent (getApplicationContext(), coloriageActivity.class);
             startActivity(autreact);
